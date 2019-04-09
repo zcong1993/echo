ddcff900128b2029ed742314bdd0bdadf66bcff6
zcong1993 <zhangcong1992@gmail.com>
2019-04-09 15:57:36 +0800

    use go mod and return ip

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/zcong1993/echo
+
+go 1.12
+
+require (
+	github.com/gin-contrib/cors v0.0.0-20170318125340-cf4846e6a636
+	github.com/gin-contrib/sse v0.0.0-20170109093832-22d885f9ecc7
+	github.com/gin-gonic/gin v1.3.0
+	github.com/golang/protobuf v0.0.0-20170601230230-5a0f697c9ed9
+	github.com/gorilla/websocket v1.3.0
+	github.com/json-iterator/go v0.0.0-20170829155851-36b14963da70
+	github.com/mattn/go-isatty v0.0.0-20170307163044-57fdcb988a5c
+	github.com/ugorji/go v0.0.0-20170215201144-c88ee250d022
+	golang.org/x/sys v0.0.0-20180824143301-4910a1d54f87
+	gopkg.in/go-playground/validator.v8 v8.18.1
+	gopkg.in/yaml.v2 v2.0.0-20160928153709-a5b47d31c556
+)
--- main.go
+++ main.go
@@ -1,14 +1,92 @@
 package main
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"net/url"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
-	"io"
-	"log"
-	"net/http"
 )
+
+// GetClientIPHelper gets the client IP using a mixture of techniques.
+// This is how it is with golang at the moment.
+func GetClientIPHelper(req *http.Request) (ipResult string, errResult error) {
+
+	// Try lots of ways :) Order is important.
+
+	//  Try Request Header ("Origin")
+	url, err := url.Parse(req.Header.Get("Origin"))
+	if err == nil {
+		host := url.Host
+		ip, _, err := net.SplitHostPort(host)
+		if err == nil {
+			return ip, nil
+		}
+	}
+
+	// Try by Request
+	ip, err := getClientIPByRequestRemoteAddr(req)
+	if err == nil {
+		return ip, nil
+	}
+
+	// Try Request Headers (X-Forwarder). Client could be behind a Proxy
+	ip, err = getClientIPByHeaders(req)
+	if err == nil {
+		return ip, nil
+	}
+
+	err = errors.New("error: Could not find clients IP address")
+	return "", err
+}
+
+// getClientIPByRequest tries to get directly from the Request.
+// https://blog.golang.org/context/userip/userip.go
+func getClientIPByRequestRemoteAddr(req *http.Request) (ip string, err error) {
+
+	// Try via request
+	ip, _, err = net.SplitHostPort(req.RemoteAddr)
+	if err != nil {
+		return "", err
+	}
+
+	userIP := net.ParseIP(ip)
+	if userIP == nil {
+		message := fmt.Sprintf("debug: Parsing IP from Request.RemoteAddr got nothing.")
+		return "", fmt.Errorf(message)
+
+	}
+	return userIP.String(), nil
+
+}
+
+// getClientIPByHeaders tries to get directly from the Request Headers.
+// This is only way when the client is behind a Proxy.
+func getClientIPByHeaders(req *http.Request) (ip string, err error) {
+
+	// Client could be behid a Proxy, so Try Request Headers (X-Forwarder)
+	ipSlice := []string{}
+
+	ipSlice = append(ipSlice, req.Header.Get("X-Forwarded-For"))
+	ipSlice = append(ipSlice, req.Header.Get("x-forwarded-for"))
+	ipSlice = append(ipSlice, req.Header.Get("X-FORWARDED-FOR"))
+
+	for _, v := range ipSlice {
+		if v != "" {
+			return v, nil
+		}
+	}
+	err = errors.New("error: Could not find clients IP address from the Request Headers")
+	return "", err
+
+}
 
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
@@ -39,11 +117,13 @@ func wsHandler(c *gin.Context) {
 }
 
 func helloHandler(c *gin.Context) {
-	res := ""
+	var res bytes.Buffer
 	for k, v := range c.Request.Header {
-		res += fmt.Sprintf("%s:%s\n", k, v)
+		res.WriteString(fmt.Sprintf("%s:%s\n", k, v))
 	}
-	c.String(http.StatusOK, res)
+	ip, _ := GetClientIPHelper(c.Request)
+	res.WriteString(fmt.Sprintf("ip: %s\n", ip))
+	c.String(http.StatusOK, res.String())
 }
 
 func echoHandler(c *gin.Context) {
